2ca034be7682e63d4281c3cacb4f7f705990e226
Ryan Andrews <155686301+TwlvStix@users.noreply.github.com>
2026-01-06 07:54:10 -0800

    Add Firebase emulator wiring for debug

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -1,5 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_functions/firebase_functions.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
+
+const bool _useFirebaseEmulator =
+    bool.fromEnvironment('USE_FIREBASE_EMULATOR', defaultValue: false);
+const int _authEmulatorPort =
+    int.fromEnvironment('FIREBASE_AUTH_EMULATOR_PORT', defaultValue: 9099);
+const int _firestoreEmulatorPort =
+    int.fromEnvironment('FIRESTORE_EMULATOR_PORT', defaultValue: 8080);
+const int _functionsEmulatorPort =
+    int.fromEnvironment('FIREBASE_FUNCTIONS_EMULATOR_PORT', defaultValue: 5001);
+const int _storageEmulatorPort =
+    int.fromEnvironment('FIREBASE_STORAGE_EMULATOR_PORT', defaultValue: 9199);
 
 Future initFirebase() async {
   if (kIsWeb) {
@@ -13,5 +28,33 @@ Future initFirebase() async {
             appId: "1:357406229935:web:2e7fb49282aad25ce9db5a"));
   } else {
     await Firebase.initializeApp();
+  }
+
+  if (kDebugMode && _useFirebaseEmulator) {
+    final emulatorHost = _getEmulatorHost();
+    FirebaseAuth.instance.useAuthEmulator(emulatorHost, _authEmulatorPort);
+    FirebaseFirestore.instance
+        .useFirestoreEmulator(emulatorHost, _firestoreEmulatorPort);
+    FirebaseFunctions.instanceFor(region: 'us-west2')
+        .useFunctionsEmulator(emulatorHost, _functionsEmulatorPort);
+    FirebaseStorage.instance
+        .useStorageEmulator(emulatorHost, _storageEmulatorPort);
+  }
+}
+
+String _getEmulatorHost() {
+  if (kIsWeb) {
+    return 'localhost';
+  }
+
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      return '10.0.2.2';
+    case TargetPlatform.iOS:
+    case TargetPlatform.macOS:
+    case TargetPlatform.windows:
+    case TargetPlatform.linux:
+    case TargetPlatform.fuchsia:
+      return 'localhost';
   }
 }
