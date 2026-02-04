01c9bfe1dcf557e9cd001f4860e2537f1688395c
TwlvStix <155686301+TwlvStix@users.noreply.github.com>
2026-02-03 21:28:10 -0800

    fix: restore font_awesome_flutter and google_fonts imports
    These imports are actually used in main_profile_widget and should not
    have been removed in batch-2.

--- lib/profile/main_profile/main_profile_widget.dart
+++ lib/profile/main_profile/main_profile_widget.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '/auth/firebase_auth/auth_util.dart';
 import '/core/widgets/app_icon_button.dart';
